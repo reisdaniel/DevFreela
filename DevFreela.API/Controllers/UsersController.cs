8d2afe6cd9b869f843a64968dd1ef2d66e8c086a
Daniel <danielmedeiros.dev@outlook.com>
2024-06-18 17:11:09 -0300

    Adiciona UsersController, CreateUserModel, CreateCommentModel, LoginModel e atualiza ProjectsController

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserModel createUser)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 1}, createUser);
+        }
+
+        [HttpPut("{id}/login")]
+        public IActionResult Login(in id, [FromBody] LoginModel login)
+        {
+            return NoContent();
+        }
+    }
+}
