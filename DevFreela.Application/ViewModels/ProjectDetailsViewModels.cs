7c50292d12f674402ad06dfa93bc1454ac7b4858
Daniel <danielmedeiros.dev@outlook.com>
2024-06-25 15:59:22 -0300

    Implementa serviços e modelos no projeto DevFreela.Application

--- DevFreela.Application/ViewModels/ProjectDetailsViewModels.cs
+++ DevFreela.Application/ViewModels/ProjectDetailsViewModels.cs
@@ -8,5 +8,21 @@ namespace DevFreela.Application.ViewModels
 {
     public class ProjectDetailsViewModels
     {
+        public ProjectDetailsViewModels(int id, string ttile, string description, decimal totalCost, DateTime? startedAt, DateTime? finisheAt)
+        {
+            Id = id;
+            Ttile = ttile;
+            Description = description;
+            TotalCost = totalCost;
+            StartedAt = startedAt;
+            FinisheAt = finisheAt;
+        }
+
+        public int Id { get; set; }
+        public string Ttile { get; set; }
+        public string Description { get; private set; }
+        public decimal TotalCost { get; set; }
+        public DateTime? StartedAt { get; private set; }
+        public DateTime? FinisheAt { get; private set; }
     }
 }
--- DevFreela.Application/ViewModels/ProjectViewModel.cs
+++ DevFreela.Application/ViewModels/ProjectViewModel.cs
@@ -8,5 +8,13 @@ namespace DevFreela.Application.ViewModels
 {
     public class ProjectViewModel
     {
+        public ProjectViewModel(string title, DateTime createdAt)
+        {
+            Title = title;
+            CreatedAt = createdAt;
+        }
+
+        public string Title { get; private set; }
+        public DateTime CreatedAt { get; private set; }
     }
 }
