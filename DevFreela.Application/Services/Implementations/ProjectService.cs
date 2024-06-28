9d99bcbd2cc78c081f1b375b05787bddcccd1b6c
Daniel <danielmedeiros.dev@outlook.com>
2024-06-27 22:13:18 -0300

    Realiza ajustes gerais e atualizações de dependências

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -48,7 +48,7 @@ namespace DevFreela.Application.Services.Implementations
             var projects = _dbContext.Projects;
 
             var projectsViewModel = projects
-                .Select(p => new ProjectViewModel(p.Title, p.CreatedAt))
+                .Select(p => new ProjectViewModel(p.Id, p.Title, p.CreatedAt))
                 .ToList();
 
             return projectsViewModel;
@@ -57,7 +57,9 @@ namespace DevFreela.Application.Services.Implementations
         public ProjectDetailsViewModels GetById(int id)
         {
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
-           
+
+            if (project == null) return null;
+
             var projectDetailsViewModel = new ProjectDetailsViewModels(
                 project.Id,
                 project.Title,
